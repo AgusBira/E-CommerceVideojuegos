7438d9143176d809382e89d95ce4afccd4d0bda7
Agus Bira <agusbira398@gmail.com>
2025-03-07 15:10:51 -0300

    cambio de logo

--- src/components/LogoComponent.jsx
+++ src/components/LogoComponent.jsx
@@ -1,9 +1,8 @@
-import logo from "../assets/logo.png"
 import "./logoComponent.css"
 export default function LogoComponent (){
     return(
         <>
-           <button className="logoComponent"><img src={logo} alt="" /></button>
+           <button className="logoComponent"><img src="https://png.pngtree.com/png-vector/20190330/ourmid/pngtree-vector-house-icon-png-image_889944.jpg" alt="" /></button>
         </>
     )
     
